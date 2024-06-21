dcb8e914349b587c05a9df871c7a26ad9273c80b
hungpv <phanhung2702@gmail.com>
2024-06-21 10:18:18 +0700

    [Add] slider homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 import Login from './views/Login'
+import Home from './views/Home'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={(
+        <Login />
+        )} />
+      <Route path="/home" element={(
+        <Home />
+        )} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  )
+}
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <AppRoutes />
+    </Router>
   )
 }
 
