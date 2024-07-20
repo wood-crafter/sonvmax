2c7a7acfeedd333970a5199c68ec8fb5340aaf50
hungpv <phanhung2702@gmail.com>
2024-07-20 15:35:47 +0700

    feat: user store persist with sessionStorage

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,20 +1,29 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface UserState {
-  accessToken: string
-  roleName: string
-  setAccessToken: (token: string) => void
-  removeAccessToken: () => void
+  accessToken: string;
+  roleName: string;
+  setAccessToken: (accessToken: string) => void;
+  removeAccessToken: () => void;
 }
 
-export const useUserStore = create<UserState>((set) => ({
-  accessToken: '',
-  roleName: '',
-  setAccessToken: (token: string) => {
-    set(() => ({ accessToken: token }))
-    const payload = token.split('.')[1]
-    const roleName = JSON.parse(atob(payload)).roleName
-    set(() => ({ roleName: roleName }))
-  },
-  removeAccessToken: () => set({ accessToken: '' }),
-}))
+export const useUserStore = create(
+  persist<UserState>(
+    (set) => ({
+      accessToken: "",
+      roleName: "",
+      setAccessToken: (accessToken: string) => {
+        const payload = accessToken.split(".")[1];
+        const roleName = JSON.parse(atob(payload)).roleName;
+
+        set(() => ({ roleName, accessToken }));
+      },
+      removeAccessToken: () => set({ accessToken: "" }),
+    }),
+    {
+      name: "@sonvmax/user-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
