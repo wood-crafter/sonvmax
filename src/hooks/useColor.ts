5ab372eebbb8302180c68b422cacd1b09ac39aae
hungpv <phanhung2702@gmail.com>
2024-08-07 17:37:49 +0700

    [Delete] remove unused function groupColorsByChildType

--- src/hooks/useColor.ts
+++ src/hooks/useColor.ts
@@ -9,47 +9,11 @@ export async function fetchColors(url: string) {
   return res.json() as Promise<{ colors: ParentColor[] }>
 }
 
-const groupColorsByChildType = (colors: ParentColor[] | undefined): ParentColor[][] => {
-  const groupedColors: { [key: string]: ParentColor[] } = {};
-  if (!colors) return [];
-
-  // Initialize each group with all parent colors
-  const colorTypes = new Set<string>();
-  colors.forEach(parentColor => {
-    parentColor.childs.forEach(child => {
-      colorTypes.add(child.colorType);
-    });
-  });
-
-  colorTypes.forEach(type => {
-    groupedColors[type] = colors.map(parentColor => ({
-      name: parentColor.name,
-      rgb: parentColor.rgb,
-      childs: [],
-      type: type,
-    }));
-  });
-
-  // Add child colors to the appropriate parent in each group
-  colors.forEach((parentColor) => {
-    parentColor.childs.forEach((child) => {
-      const parentColorGroup = groupedColors[child.colorType];
-      const existingParent = parentColorGroup.find(pc => pc.name === parentColor.name);
-      if (existingParent) {
-        existingParent.childs.push(child);
-      }
-    });
-  });
-
-  // Convert the grouped colors object to an array of arrays
-  return Object.values(groupedColors);
-}
-
 export function useColors() {
   const { data, isLoading, error, mutate } = useSWR(`/color/get-color`, fetchColors)
 
   return {
-    data: groupColorsByChildType(data?.colors),
+    data: data,
     isLoading,
     error,
     mutate,
