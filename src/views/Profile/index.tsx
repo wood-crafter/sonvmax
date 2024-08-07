a5c06cd86f4a8930bfbc65d28a7a4952a1f9d4bb
nlam281199 <boombieone@gmail.com>
2024-08-07 17:20:53 +0700

    [Fix] overcome build error

--- src/views/Profile/index.tsx
+++ src/views/Profile/index.tsx
@@ -2,7 +2,7 @@ import { useAgentById } from "../../hooks/useAgent";
 import { useUserStore } from "../../store/user";
 import "./index.css";
 function Profile() {
-  const roleName = useUserStore((state) => state.roleName);
+  // const roleName = useUserStore((state) => state.roleName);
   const id = useUserStore((state) => state.id);
   const { data } = useAgentById(id);
   console.info(data);
