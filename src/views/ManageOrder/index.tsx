92cdf4cf2cd85e2fdeb0f2c074cc0f7ae9589a25
hungpv <phanhung2702@gmail.com>
2024-08-12 01:28:06 +0700

    [Add] create invoice

--- src/views/ManageOrder/index.tsx
+++ src/views/ManageOrder/index.tsx
@@ -230,6 +230,29 @@ function ManageOrder() {
     }
   };
 
+  const createInvoice = async (id: string) => {
+    const createInvoiceRes = await authFetch(`${API_ROOT}/invoice/${id}`, {
+      ...requestOptions,
+      method: "POST",
+      headers: {
+        ...requestOptions.headers,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (createInvoiceRes.ok) {
+      api.open({
+        message: "Tạo hóa đơn thành công",
+        icon: <SmileOutlined style={{ color: "#108ee9" }} />,
+      });
+    } else {
+      api.open({
+        message: "Tạo hóa đơn thất bại",
+        icon: <FrownOutlined style={{ color: "red" }} />,
+      });
+    }
+  };
+
   const columns: ColumnType<Order>[] = [
     {
       title: "id",
@@ -359,6 +382,13 @@ function ManageOrder() {
                 }}
               >
                 Sửa
+              </Button>
+              <Button
+                onClick={() => {
+                  createInvoice(record.id);
+                }}
+              >
+                Tạo hóa đơn
               </Button>
             </>
           )}
