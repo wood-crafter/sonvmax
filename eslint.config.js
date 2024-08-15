6f78256ee0aea30a5f9a16dc47ac30d522da97e1
hungpv <phanhung2702@gmail.com>
2024-08-15 18:39:10 +0700

    [Add] ESLint rule for React hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import { fixupPluginRules } from "@eslint/compat";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -9,10 +11,16 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
+  {
+    plugins: {
+      "react": eslintPluginReact,
+      "react-hooks": fixupPluginRules(eslintPluginReactHooks),
+    }
+  },
   {
     rules: {
       "react/react-in-jsx-scope": "off",
+      ...eslintPluginReactHooks.configs.recommended.rules,
     },
   },
 ];
