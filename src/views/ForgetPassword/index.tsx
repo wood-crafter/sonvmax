a35a8f71fdf0db6b9fab6471d64b30c91ad1280f
hungpv <phanhung2702@gmail.com>
2024-08-20 02:55:31 +0700

    [Add] forget password

--- src/views/ForgetPassword/index.tsx
+++ src/views/ForgetPassword/index.tsx
@@ -1,19 +1,70 @@
 import { Link } from "react-router-dom";
 import "./index.css";
+import { Form, Input, notification } from "antd";
+import { useState } from "react";
+import { SmileOutlined, FrownOutlined } from "@ant-design/icons";
+import { requestOptions } from "../../hooks/utils";
+import { API_ROOT } from "../../constant";
+import { useAuthenticatedFetch } from "../../hooks/useAuthenticatedFetch";
 
 function ForgetPassword() {
+  const authFetch = useAuthenticatedFetch();
+  const [api, contextHolder] = notification.useNotification();
+  const [email, setEmail] = useState("");
+
+  const handleForgetPassword = async () => {
+    if (!email) {
+      api.open({
+        message: "Vui lòng nhập email",
+        icon: <FrownOutlined style={{ color: "red" }} />,
+      });
+      return;
+    }
+    const forgetPasswordRes = await authFetch(
+      `${API_ROOT}/auth/forgot-password`,
+      {
+        ...requestOptions,
+        body: JSON.stringify({ email: email }),
+        method: "POST",
+        headers: {
+          ...requestOptions.headers,
+        },
+      }
+    );
+
+    if (forgetPasswordRes.ok) {
+      api.open({
+        message: `Mật khẩu đã mới đã được gửi trong email: ${email}`,
+        icon: <SmileOutlined style={{ color: "blue" }} />,
+      });
+    } else {
+      api.open({
+        message: `Email đã nhập sai`,
+        icon: <FrownOutlined style={{ color: "red" }} />,
+      });
+    }
+  };
   return (
     <div className="ForgetPassword">
-      <div className="container">
+      {contextHolder}
+      <Form className="container" onFinish={handleForgetPassword}>
         <div className="title">Quên mật khẩu</div>
-        <input className="input-email" type="email" placeholder="Email" />
+        <Form.Item style={{ width: "70%" }}>
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="input-email"
+            type="email"
+            placeholder="Email"
+          />
+        </Form.Item>
         <div className="action">
-          <button>Gửi mail</button>
+          <button type="submit">Gửi mail</button>
           <Link to="/login">
             <button>Đăng nhập</button>
           </Link>
         </div>
-      </div>
+      </Form>
     </div>
   );
 }
